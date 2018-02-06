f079dca8dc9663818ba0b92f2b22dec433af9f2f
antiline <antiline@jun2.org>
2018-02-07 01:13:12 +0900

    dateformat 추가

---
+++ src/lib/utils/format.py
@@ -0,0 +1,6 @@
+class DateTimeFormat(object):
+    YMD_HMSM = '%Y-%m-%d %H:%M:%S%f'
+    YMD_HMS = '%Y-%m-%d %H:%M:%S'
+    YMD_HM = '%Y-%m-%d %H:%M'
+    YMD = '%Y-%m-%d'
+    SHORT_YMD = '%y%m%d'
--- src/sites/settings/base.py
+++ src/sites/settings/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.domains.home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
