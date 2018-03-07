eb5f16b20b3b76ac35fa3f9f47815baf74ae92a9
antiline <antiline@jun2.org>
2018-03-07 23:50:05 +0900

    RequestBalancer 통해서 1초당 요청되는 양 조절

--- src/lib/naver/api.py
+++ src/lib/naver/api.py
@@ -1,14 +1,30 @@
+import time
+from time import sleep
 from typing import Type
 
 import requests
 import xmltodict
 from django.conf import settings
 
-from lib.naver.dtos import NaverBookSearchApiResult, BaseNaverSearchApiResult, NaverSearchApiResult
+from lib.naver.dtos import BaseNaverSearchApiResult, NaverBookSearchApiResult, NaverSearchApiResult
 from lib.naver.exceptions import NaverWrongRequest
 
 MAX_SEARCH_RESULT_COUNT = 100
 SEARCH_BOOK_URL = 'https://openapi.naver.com/v1/search/book.xml'
+ALLOWED_REQ_COUNT_IN_SEC = 10
+
+
+class RequestBalancer:
+    _count = {}
+
+    @classmethod
+    def ready(cls):
+        timestamp = int(time.time())
+
+        if cls._count[timestamp] > ALLOWED_REQ_COUNT_IN_SEC:
+            sleep(0.5)
+            cls._count.clear()
+        cls._count[timestamp] += 1
 
 
 def search_book(query: str):
@@ -21,6 +37,8 @@ def search_book(query: str):
 
 
 def _call(ditme: Type[BaseNaverSearchApiResult], url: str, params: dict) -> NaverSearchApiResult:
+    RequestBalancer.ready()
+
     headers = {
         'X-Naver-Client-Id': settings.NAVER_OPEN_API_CLIENT_ID,
         'X-Naver-Client-Secret': settings.NAVER_OPEN_API_CLIENT_SECRET,
