3dc392bd3c77b80d1bc5026be29307fbfe5033c6
antiline <antiline@jun2.org>
2018-03-08 00:17:58 +0900

    매일 새벽 2시에 신간 크롤링 하도록 함

--- src/infra/crontab.py
+++ src/infra/crontab.py
@@ -4,6 +4,6 @@ from django.core.management import call_command
 __all__ = ('ping', )
 
 
-@cron(minute=0, hour=-1, day=-1, month=-1, dayweek=-1,)
+@cron(minute=0, hour=2, day=-1, month=-1, dayweek=-1,)
 def ping():
-    call_command('ping')
+    call_command('crawling_new_book')
