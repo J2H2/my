6dbbd9eba237ed4940838c08f94277be65da9024
antiline <antiline@jun2.org>
2018-03-08 00:46:51 +0900

    저장시 desc 에 mb4 형식이 될 수 있는데, 관련 수정이 필요함

--- src/apps/domains/book/services/search_book_service.py
+++ src/apps/domains/book/services/search_book_service.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from django.db import OperationalError
+
 from apps.domains.book.models import Book
 from lib.naver.api import search_book
 
@@ -10,18 +12,24 @@ class SearchBookService:
         _books = []
         result = search_book(keyword)
         for item in result.items:
-            _books = Book.objects.get_or_create(
-                isbn=item.isbn,
-                defaults={
-                    'title': item.title,
-                    'cover_s': item.image,
-                    'authors': item.author,
-                    'publisher': item.publisher,
-                    'description': item.description,
-                    'pub_date': item.pubdate,
-                    'price': item.price,
-                }
-            )
+            try:
+                _book, _ = Book.objects.get_or_create(
+                    isbn=item.isbn,
+                    defaults={
+                        'title': item.title,
+                        'cover_s': item.image,
+                        'authors': item.author,
+                        'publisher': item.publisher,
+                        'description': item.description,
+                        'pub_date': item.pubdate,
+                        'price': item.price,
+                    }
+                )
+            except OperationalError:
+                # TODO logger 추가
+                # TODO desc 에 mb4형식이 있을 수 있음. desc 정리를 하거나 DB설정을 바꾸거나 해야 함
+                continue
+            _books.append(_book)
 
         return _books
 
