97e28c45b8708b61cb6fc00ce37e7af98a91a874
antiline <antiline@jun2.org>
2018-05-07 04:20:07 +0900

    fix cron path

--- src/infra/cronjob.py
+++ src/infra/cronjob.py
@@ -1,9 +1,9 @@
 from django.core.management import call_command
 from uwsgidecorators import cron
 
-__all__ = ('ping',)
+__all__ = ('crawling_new_book',)
 
 
 @cron(minute=0, hour=2, day=-1, month=-1, dayweek=-1, )
-def ping():
+def crawling_new_book():
     call_command('crawling_new_book')
