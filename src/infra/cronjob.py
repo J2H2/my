fbe7e8191f6cc6c8b6292772e6b42dfb328204f1
antiline <antiline@jun2.org>
2018-05-20 14:46:00 +0900

    Fix uwsgi cron error

--- src/infra/cronjob.py
+++ src/infra/cronjob.py
@@ -1,9 +1,7 @@
 from django.core.management import call_command
 from uwsgidecorators import cron
 
-__all__ = ('crawling_new_book',)
-
 
 @cron(minute=0, hour=2, day=-1, month=-1, dayweek=-1, )
-def crawling_new_book():
+def crawling_new_book(signum: int):
     call_command('crawling_new_book')
